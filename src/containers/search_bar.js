1fce59c4256262dffb1b68f6c7529b3eec1ff8f1
leonardo zapparoli <leo.zapparoli@gmail.com>
2017-04-19 15:41:54 -0300

    first commit

---
+++ src/containers/search_bar.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: '' };
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+ }
+  
+  onInputChange(event) {
+    this.setState({term: event.target.value});
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    
+    // fetch weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({term: ''});
+  }
+
+  render() {
+    return (
+      <form
+        onSubmit={this.onFormSubmit}
+        className="input-group">
+        <input 
+            placeholder="Give a five-day forecast in your favorite cities"
+            className="form-control"
+            onChange={this.onInputChange}
+            value={this.state.term} />
+        <span className="input-group-btn">
+          <button 
+            type="submit"
+            className="btn btn-secondary">Submit
+          </button>
+        </span>
+      </form>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
